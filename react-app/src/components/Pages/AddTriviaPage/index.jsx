483a1513cac94b8e43e49174c10467fb29a789c9
Daniel Yoo <dacapo0509@gmail.com>
2023-01-24 14:50:45 -0800

    working version

--- react-app/src/components/Pages/AddTriviaPage/index.jsx
+++ react-app/src/components/Pages/AddTriviaPage/index.jsx
@@ -1,9 +1,6 @@
-import React from "react";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import AddTriviaCardModal from "../../Modals/AddTriviaCard/AddTriviaCardModal";
+
 const AddTriviaPage = () => {
-	const [isOpenAddTriviaCard, setIsOpenAddTriviaCard] = useState(false);
 	const sessionUser = useSelector((state) => state.session.user);
 
 	return (
@@ -11,15 +8,6 @@ const AddTriviaPage = () => {
 			{sessionUser && (
 				<div>
 					<h1>THIS IS TO ADD TRIVIA PAGE</h1>
-					{isOpenAddTriviaCard && (
-						<AddTriviaCardModal
-							setIsOpen={setIsOpenAddTriviaCard}
-							sessionUser={sessionUser}
-						/>
-					)}
-					<button onClick={() => setIsOpenAddTriviaCard(true)}>
-						Create Trivia!
-					</button>
 				</div>
 			)}
 		</div>
