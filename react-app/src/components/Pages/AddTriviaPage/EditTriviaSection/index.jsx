e6b6fa6420b2fb373b2d41b417cc539d48e50ee7
Daniel Yoo <dacapo0509@gmail.com>
2023-01-25 13:30:47 -0800

    this isnt working. need to figure out how to edit the questions with the right id

--- react-app/src/components/Pages/AddTriviaPage/EditTriviaSection/index.jsx
+++ react-app/src/components/Pages/AddTriviaPage/EditTriviaSection/index.jsx
@@ -4,57 +4,58 @@ import { useHistory } from "react-router-dom";
 import { getAllTriviasThunk } from "../../../../store/trivia";
 import EditTriviaModal from "../../../Modals/EditTrivia/EditTriviaModal";
 
-const EditTriviaForm = ({ sessionUser, triviapackage }) => {
+const EditTriviaForm = ({ sessionUser, triviaId }) => {
 	const dispatch = useDispatch();
-	const history = useHistory();
-	const allTrivia = useSelector((state) => Object.values(state.trivias));
+	const trivia = useSelector((state) => state.trivias[triviaId]);
 
-	const selectedTrivia = allTrivia?.filter((trivia) => {
-		return triviapackage?.id === trivia?.triviaPackageId;
-	});
+	const history = useHistory();
+	// const allTrivia = useSelector((state) => Object.values(state.trivias));
+
+	// const selectedTrivia = allTrivia?.filter((trivia) => {
+	// 	return triviapackage?.id === trivia?.triviaPackageId;
+	// });
 	const [isOpenEdit, setIsOpenEdit] = useState(false);
 
 	// if (triviapackage?.userId !== sessionUser?.id) {
 	// 	history.push("/");
 	// }
 
-	useEffect(() => {
-		dispatch(getAllTriviasThunk());
-	}, [dispatch]);
-
-	console.log(selectedTrivia);
+	// useEffect(() => {
+	// 	dispatch(getAllTriviasThunk());
+	// }, [dispatch]);
 	return (
-		<div>
-			{selectedTrivia && selectedTrivia.length > 0 ? (
-				<div>
-					{selectedTrivia.map((trivia) => (
-						<div key={trivia.id}>
-							<div>
-								<p>Question: {trivia.question}</p>
-								<p>Answer: {trivia.correctAnswer}</p>
-							</div>
-							<div>
-								<button onClick={() => setIsOpenEdit(true)}>
-									Edit
-								</button>
-								<button>Delete</button>
-							</div>
-							{isOpenEdit && (
-								<EditTriviaModal
-									setIsOpen={setIsOpenEdit}
-									trivia={trivia}
-									sessionUser={sessionUser}
-								/>
-							)}
-						</div>
-					))}
-				</div>
-			) : (
-				<div>
-					<h1>No trivia questions yet!</h1>
-				</div>
-			)}
-		</div>
+		<h2>hi</h2>
+		// <div>
+		// 	{selectedTrivia && selectedTrivia.length > 0 ? (
+		// 		<div>
+		// 			{selectedTrivia.map((trivia) => (
+		// 				<div key={trivia.id}>
+		// 					<div>
+		// 						<p>Question: {trivia.question}</p>
+		// 						<p>Answer: {trivia.correctAnswer}</p>
+		// 					</div>
+		// 					<div>
+		// 						<button onClick={() => setIsOpenEdit(true)}>
+		// 							Edit
+		// 						</button>
+		// 						<button>Delete</button>
+		// 					</div>
+		// 					{isOpenEdit && (
+		// 						<EditTriviaModal
+		// 							setIsOpen={setIsOpenEdit}
+		// 							trivia={trivia}
+		// 							sessionUser={sessionUser}
+		// 						/>
+		// 					)}
+		// 				</div>
+		// 			))}
+		// 		</div>
+		// 	) : (
+		// 		<div>
+		// 			<h1>No trivia questions yet!</h1>
+		// 		</div>
+		// 	)}
+		// </div>
 	);
 };
 
--- react-app/src/components/Pages/AddTriviaPage/index.jsx
+++ react-app/src/components/Pages/AddTriviaPage/index.jsx
@@ -2,20 +2,25 @@ import { useParams } from "react-router-dom";
 import EditTriviaForm from "./EditTriviaSection";
 import AddTriviaForm from "./AddTriviaSection";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllTriviasThunk } from "../../../store/trivia";
 const AddTriviaPage = () => {
 	const { triviapackageId } = useParams();
 	const trivPackageId = parseInt(triviapackageId);
+	const dispatch = useDispatch();
 	const location = useLocation();
 	const sessionUser = location.state?.sessionUser;
 	const triviapackage = location.state?.triviapackage;
-
 	const allTrivias = useSelector((state) => Object.values(state.trivias));
-	console.log(allTrivias);
-	// const selectedTrivias = allTrivias.filter(trivia=> {
-	// 	return
-	// })
+	const selectedTrivias = allTrivias.filter((trivia) => {
+		return trivia.triviaPackageId === trivPackageId;
+	});
+	console.log(selectedTrivias, "these are all the selected Trivias");
 
+	useEffect(() => {
+		getAllTriviasThunk();
+	}, []);
 	return (
 		<div>
 			{sessionUser && triviapackage && (
@@ -29,13 +34,25 @@ const AddTriviaPage = () => {
 					triviapackage={triviapackage}
 				/>
 			</div>
+			<div id="add-trivia-page-edit-container">
+				{selectedTrivias &&
+					selectedTrivias.map((trivia) => (
+						<div id={trivia.id}>
+							<EditTriviaForm
+								// vendor={vendor}
+								triviaId={trivia.id}
+								sessionUser={sessionUser}
+							/>
+						</div>
+					))}
+			</div>
 
-			<div className="add-trivia-page-edit-container">
+			{/* <div className="add-trivia-page-edit-container">
 				<EditTriviaForm
 					sessionUser={sessionUser}
 					triviapackage={triviapackage}
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 };
