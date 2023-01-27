6316dd6793fc16cd5b0f25c8dd9a0d880e473088
Daniel Yoo <dacapo0509@gmail.com>
2023-01-27 14:49:10 -0800

    added the triviapackage seeds

--- app/seeds/triviapackages.py
+++ app/seeds/triviapackages.py
@@ -18,7 +18,7 @@ def seed_trivia_packages():
     triviapackage7 = TriviaPackage(
         name="Everyone and Everything", category="General Knowledge", description="Lets get it! How much do you actually know?", difficulty="Easy", user_id=3, image_url="https://trivia-fighter.s3.us-west-2.amazonaws.com/Images/videogamefatcat.jpg" )
   
-    db.session.add_all([triviapackage1])
+    db.session.add_all([triviapackage1,triviapackage2,triviapackage3,triviapackage4,triviapackage5,triviapackage6,triviapackage7])
 
     db.session.commit()
 
