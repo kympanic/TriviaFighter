c0c43daca1cafbc7735bd29803728d376a3fd290
Daniel Yoo <dacapo0509@gmail.com>
2023-02-05 21:46:04 -0800

    testingout why triviapackages does not work on render but locally its fine

--- app/forms/triviapackages_form.py
+++ app/forms/triviapackages_form.py
@@ -23,16 +23,16 @@ def difficulty_check(form,field):
         raise ValidationError('Provide valid difficulty')
 
 
-def triviapackage_name_exists(form, field):
-    # Checking if username is already in use
-    name = field.data
-    triviapackage = TriviaPackage.query.filter(TriviaPackage.name == name).first()
-    if triviapackage:
-        raise ValidationError('Name is already in use')
+# def triviapackage_name_exists(form, field):
+#     # Checking if username is already in use
+#     name = field.data
+#     triviapackage = TriviaPackage.query.filter(TriviaPackage.name == name).first()
+#     if triviapackage:
+#         raise ValidationError('Name is already in use')
 
 
 class TriviaPackageForm(FlaskForm):
-    user_id=IntegerField('user_id', validators=[DataRequired(),triviapackage_name_exists])
+    user_id=IntegerField('user_id', validators=[DataRequired()])
     name = StringField('name', validators=[DataRequired()])
     category=StringField('account_number', validators=[DataRequired(),category_check])
     description=StringField('description', validators=[DataRequired()])
