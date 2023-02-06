03b1cc6c073f18470a4ca7962605097ab89375f4
Daniel Yoo <dacapo0509@gmail.com>
2023-02-05 21:20:22 -0800

    troubleshooting why add and edit triviapackage works locally but not on render page

--- react-app/src/components/Modals/AddTriviaPackage/AddTriviaPackageModal.jsx
+++ react-app/src/components/Modals/AddTriviaPackage/AddTriviaPackageModal.jsx
@@ -15,6 +15,8 @@ const AddTriviaPackageModal = ({ setIsOpen, sessionUser }) => {
 	const [description, setDescription] = useState("");
 	const [image, setImage] = useState("");
 
+	console.log(sessionUser, "this is the session user");
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
