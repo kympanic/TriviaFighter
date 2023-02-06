1c84023fc6c169f30421048d119cc40c4da6f6ac
Daniel Yoo <dacapo0509@gmail.com>
2023-02-05 20:27:44 -0800

    finished rough splash page

--- react-app/src/components/Helpers/index.jsx
+++ react-app/src/components/Helpers/index.jsx
@@ -73,7 +73,7 @@ export const player2Stats = [
 	{
 		id: 5,
 		maxHealth: 100,
-		name: "Angry Kangaroo",
+		name: "Scrappy Roo",
 		img: "https://trivia-fighter.s3.us-west-2.amazonaws.com/Images/fightingkangaroo.jpg",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
--- react-app/src/components/Pages/SplashPage/index.jsx
+++ react-app/src/components/Pages/SplashPage/index.jsx
@@ -1,9 +1,123 @@
 import "./splashpage.css";
+import { player1Stats } from "../../Helpers";
+import { player2Stats } from "../../Helpers";
+import { useHistory } from "react-router-dom";
+
 const SpashPage = () => {
+	const history = useHistory();
+
+	const handleSignIn = () => {
+		history.push("/login");
+	};
+	const handleJoin = () => {
+		history.push("/sign-up");
+	};
+
 	return (
-		<div className="splashpage-main">
-			<div className="wrapper">
-				<h1>SPLASHPAGE</h1>
+		<div className="splash-main">
+			<div className="splash-content-wrapper">
+				<div className="splash-logo-section">
+					<h1 id="trivia-fighter-title">TRIVIA FIGHTER</h1>
+					<button onClick={handleSignIn} className="splash-log-btns">
+						LOG IN
+					</button>
+				</div>
+				<div className="splash-trivia-section">
+					<div className="splash-trivia-text-container">
+						<h1 id="splash-trivia-header-text">
+							PLAY AGAINST YOUR FRIENDS AND FAMILY
+						</h1>
+
+						<p className="splash-trivia-text">
+							TRIVIA FIGHTER IS A TURN BASED BATTLE GAME, BUT
+							INSTEAD OF FIGHTING WITH YOUR FISTS, YOU ANSWER
+							TRIVIA QUESTIONS USING YOUR WITS
+						</p>
+						<p className="splash-trivia-text">
+							CREATE YOUR OWN TRIVIA QUESTIONS OR CHOOSE COMMUNITY
+							CREATED GAMES
+						</p>
+					</div>
+					<div className="splash-trivia-img-container">
+						<img
+							className="splash-trivia-img"
+							src="https://trivia-fighter.s3.us-west-2.amazonaws.com/Images/Splash-trivia-img.png"
+							alt="splash-trivia"
+						/>
+					</div>
+				</div>
+				<div className="splash-char-section">
+					<div className="splashchar-container">
+						<img
+							className="splash-char-img"
+							src={player1Stats[0].img}
+							alt={player1Stats[0].name}
+						/>
+						<p className="splash-char-name">
+							{player1Stats[0].name}
+						</p>
+					</div>
+					<div className="splashchar-container">
+						<img
+							className="splash-char-img"
+							src={player2Stats[1].img}
+							alt={player2Stats[1].name}
+						/>
+						<p className="splash-char-name">
+							{player2Stats[1].name}
+						</p>
+					</div>
+					<div className="splashchar-container">
+						<img
+							className="splash-char-img"
+							src={player2Stats[2].img}
+							alt={player2Stats[2].name}
+						/>
+						<p className="splash-char-name">
+							{player2Stats[2].name}
+						</p>
+					</div>
+					<div className="splashchar-text-container">
+						<h1 id="splashchar-header-text">
+							CHOOSE YOUR CHARACTER
+						</h1>
+						<p className="splash-trivia-text">
+							IN OPTIONS, TWO PLAYERS CAN EACH SELECT AN AVATAR
+							FROM THE CHARACTERS LIST
+						</p>
+						<p className="splash-trivia-text">
+							BATTLE UNTIL ONE OF YOU FALLS
+						</p>
+					</div>
+				</div>
+				<div className="splash-battle-section">
+					<div className="splashbattle-text-container">
+						<h1 id="splashbattle-header-text">Time to Battle</h1>
+						<p className="splashbattle-text">
+							TAKE TURNS ANSWERING QUESTIONS, DO SOME DAMAGE
+						</p>
+						<p className="splashbattle-text">
+							BE CAREFUL. ANSWER INCORRECTLY AND YOU CAN TAKE
+							DAMAGE YOURSELF
+						</p>
+						<p></p>
+					</div>
+					<div className="splashbattle-img-container">
+						<img
+							className="splashbattle-img"
+							src="https://trivia-fighter.s3.us-west-2.amazonaws.com/Images/splash-battle-img.png"
+							alt="splash-battle"
+						/>
+					</div>
+				</div>
+				<div className="splash-join-section">
+					<div className="splashjoin-text-container">
+						<h1>SIGN UP TODAY</h1>
+						<button onClick={handleJoin} id="splash-join-btn">
+							JOIN
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
