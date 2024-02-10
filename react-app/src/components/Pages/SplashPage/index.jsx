6d1e38f25749d34c3625bcfc4862997027b324ba
Daniel Yoo <dacapo0509@gmail.com>
2024-02-09 17:51:07 -0800

    only section 8 to complete then need to do media queries

--- react-app/src/components/Pages/SplashPage/index.jsx
+++ react-app/src/components/Pages/SplashPage/index.jsx
@@ -80,8 +80,28 @@ const SplashPage = () => {
 					</p>
 				</div>
 
-				<div className="splash splash-section-6">TEST 6</div>
-				<div className="splash splash-section-7">TEST 7</div>
+				<div className="splash splash-section-6">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6 splash-6-icon"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+						/>
+					</svg>
+					<h3 className="splash-subtitle">
+						Choose between player and AI created games
+					</h3>
+				</div>
+				<div className="splash splash-section-7">
+					<button className="btn-splash7">DEMO</button>
+				</div>
 				<div className="splash splash-section-8">TEST 8</div>
 			</div>
 		</main>
